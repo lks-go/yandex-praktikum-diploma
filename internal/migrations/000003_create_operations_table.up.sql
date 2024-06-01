581489163a52c913305e680e7c4115342dbafc4c
aosumin <aosumin@domclick.ru>
2024-06-01 08:29:21 +0300

    migrations

--- internal/migrations/000003_create_operations_table.up.sql
+++ internal/migrations/000003_create_operations_table.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE operations (
     id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
-    order_id UUID NOT NULL UNIQUE,
+    order_number UUID NOT NULL,
     amount INTEGER,
 
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE INDEX order_number_idx ON operations (order_number);
--- internal/migrations/000003_operations_history.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE operations_history;
