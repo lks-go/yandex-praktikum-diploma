ec89b69dbe3ef0bd568bb7c9566e2b84d2d8afb5
lks-go <alekssum@gmail.com>
2024-06-03 22:03:16 +0300

    auth tests

--- internal/controller/handler/handler_test.go
+++ internal/controller/handler/handler_test.go
@@ -24,15 +24,13 @@ func TestRegisterUser(t *testing.T) {
 
 	cases := []struct {
 		name               string
-		requestBody        string
 		httpRequest        *http.Request
 		mock               func()
 		expectedStatusCode int
 		expectedCookie     string
 	}{
 		{
-			name:        "successful registration",
-			requestBody: `{"login":"test","password":"qwerty"}`,
+			name: "successful registration",
 			httpRequest: httptest.NewRequest(
 				http.MethodPost,
 				"https://test.ru/api/user/register",
@@ -46,8 +44,7 @@ func TestRegisterUser(t *testing.T) {
 			expectedCookie:     "auth_token=test-token",
 		},
 		{
-			name:        "409 conflict",
-			requestBody: `{"login":"test","password":"qwerty"}`,
+			name: "409 conflict",
 			httpRequest: httptest.NewRequest(
 				http.MethodPost,
 				"https://test.ru/api/user/register",
@@ -61,8 +58,7 @@ func TestRegisterUser(t *testing.T) {
 			expectedCookie:     "",
 		},
 		{
-			name:        "400 bad request",
-			requestBody: `{"login":"","password":"qwerty"}`,
+			name: "400 bad request",
 			httpRequest: httptest.NewRequest(
 				http.MethodPost,
 				"https://test.ru/api/user/register",
@@ -73,8 +69,7 @@ func TestRegisterUser(t *testing.T) {
 			expectedCookie:     "",
 		},
 		{
-			name:        "500 internal error",
-			requestBody: `{"login":"test","password":"qwerty"}`,
+			name: "500 internal error",
 			httpRequest: httptest.NewRequest(
 				http.MethodPost,
 				"https://test.ru/api/user/register",
@@ -82,14 +77,13 @@ func TestRegisterUser(t *testing.T) {
 			),
 			mock: func() {
 				serviceMock.On("RegisterUser", mock.Anything, "test", "qwerty").
-					Return("", errors.New("any not declares error")).Once()
+					Return("", errors.New("any not declared error")).Once()
 			},
 			expectedStatusCode: http.StatusInternalServerError,
 			expectedCookie:     "",
 		},
 		{
-			name:        "500 internal error(auth)",
-			requestBody: `{"login":"test","password":"qwerty"}`,
+			name: "500 internal error(auth)",
 			httpRequest: httptest.NewRequest(
 				http.MethodPost,
 				"https://test.ru/api/user/register",
@@ -118,4 +112,113 @@ func TestRegisterUser(t *testing.T) {
 		})
 	}
 
+}
+
+func TestLoginUser(t *testing.T) {
+	serviceMock := mocks.NewService(t)
+	h := handler.New(logrus.New(), serviceMock)
+
+	cases := []struct {
+		name               string
+		httpRequest        *http.Request
+		mock               func()
+		expectedStatusCode int
+		expectedCookie     string
+	}{
+		{
+			name: "successful auth",
+			httpRequest: httptest.NewRequest(
+				http.MethodPost,
+				"https://test.ru/api/user/login",
+				io.NopCloser(bytes.NewReader([]byte(`{"login":"test","password":"qwerty"}`))),
+			),
+			mock: func() {
+				serviceMock.On("AuthUser", mock.Anything, "test", "qwerty").
+					Return("test-token", nil).Once()
+			},
+			expectedStatusCode: http.StatusOK,
+			expectedCookie:     "auth_token=test-token",
+		},
+		{
+			name: "400 bad request",
+			httpRequest: httptest.NewRequest(
+				http.MethodPost,
+				"https://test.ru/api/user/login",
+				io.NopCloser(bytes.NewReader([]byte(`{"login":"test","password":""}`))),
+			),
+			mock:               func() {},
+			expectedStatusCode: http.StatusBadRequest,
+			expectedCookie:     "",
+		},
+		{
+			name: "401 unauthorized. ErrNotFound",
+			httpRequest: httptest.NewRequest(
+				http.MethodPost,
+				"https://test.ru/api/user/login",
+				io.NopCloser(bytes.NewReader([]byte(`{"login":"test","password":"qwerty"}`))),
+			),
+			mock: func() {
+				serviceMock.On("AuthUser", mock.Anything, "test", "qwerty").
+					Return("", service.ErrNotFound).Once()
+			},
+			expectedStatusCode: http.StatusUnauthorized,
+			expectedCookie:     "",
+		},
+		{
+			name: "401 unauthorized. ErrUsersPasswordNotMatch",
+			httpRequest: httptest.NewRequest(
+				http.MethodPost,
+				"https://test.ru/api/user/login",
+				io.NopCloser(bytes.NewReader([]byte(`{"login":"test","password":"qwerty"}`))),
+			),
+			mock: func() {
+				serviceMock.On("AuthUser", mock.Anything, "test", "qwerty").
+					Return("", service.ErrUsersPasswordNotMatch).Once()
+			},
+			expectedStatusCode: http.StatusUnauthorized,
+			expectedCookie:     "",
+		},
+		{
+			name: "500 internal. ErrAuth",
+			httpRequest: httptest.NewRequest(
+				http.MethodPost,
+				"https://test.ru/api/user/login",
+				io.NopCloser(bytes.NewReader([]byte(`{"login":"test","password":"qwerty"}`))),
+			),
+			mock: func() {
+				serviceMock.On("AuthUser", mock.Anything, "test", "qwerty").
+					Return("", service.ErrAuth{}).Once()
+			},
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedCookie:     "",
+		},
+		{
+			name: "500 internal",
+			httpRequest: httptest.NewRequest(
+				http.MethodPost,
+				"https://test.ru/api/user/login",
+				io.NopCloser(bytes.NewReader([]byte(`{"login":"test","password":"qwerty"}`))),
+			),
+			mock: func() {
+				serviceMock.On("AuthUser", mock.Anything, "test", "qwerty").
+					Return("", errors.New("any not declared error")).Once()
+			},
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedCookie:     "",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock()
+
+			w := httptest.NewRecorder()
+			h.LoginUser(w, tc.httpRequest)
+
+			assert.Equal(t, tc.expectedStatusCode, w.Code)
+			if tc.expectedCookie != "" {
+				assert.Equal(t, tc.expectedCookie, w.Header().Get("Set-Cookie"))
+			}
+		})
+	}
 }
